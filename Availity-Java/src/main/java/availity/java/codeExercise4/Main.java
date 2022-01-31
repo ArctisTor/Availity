ab67aaffbf71ac4dd66a392fe851620b09cacbf9
ArctisTor <cplatz@citadel.edu>
2022-01-30 20:49:25 -0500

    User input for code exercise 4.

--- Availity-Java/src/main/java/availity/java/codeExercise4/Main.java
+++ Availity-Java/src/main/java/availity/java/codeExercise4/Main.java
@@ -1,8 +1,17 @@
 package availity.java.codeExercise4;
 
+import availity.java.codeExercise4.util.ParentheseChecker;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String args[]) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a string with parenthese, ( or ). If it is valid ie it has closing parenthese the " +
+                "program will let you know.");
+        String parenthese = scan.nextLine();
+        System.out.println("The string was valid (stack process): " + ParentheseChecker.validParentheseStack(parenthese));
+        System.out.println("The string was valid: " + ParentheseChecker.validParenthese(parenthese));
     }
 }
