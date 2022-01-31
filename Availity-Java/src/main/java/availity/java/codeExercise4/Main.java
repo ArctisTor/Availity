586533c1ff4d1fcfff7e54c9b9ab71e8e8f1416e
ArctisTor <cplatz@citadel.edu>
2022-01-30 20:49:58 -0500

    I can't spell parentheses.

--- Availity-Java/src/main/java/availity/java/codeExercise4/Main.java
+++ Availity-Java/src/main/java/availity/java/codeExercise4/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a string with parenthese, ( or ). If it is valid ie it has closing parenthese the " +
+        System.out.println("Enter a string with parentheses, ( or ). If it is valid ie it has closing parentheses the " +
                 "program will let you know.");
-        String parenthese = scan.nextLine();
-        System.out.println("The string was valid (stack process): " + ParentheseChecker.validParentheseStack(parenthese));
-        System.out.println("The string was valid: " + ParentheseChecker.validParenthese(parenthese));
+        String parentheses = scan.nextLine();
+        System.out.println("The string was valid (stack process): " + ParentheseChecker.validParentheseStack(parentheses));
+        System.out.println("The string was valid: " + ParentheseChecker.validParenthese(parentheses));
     }
 }
